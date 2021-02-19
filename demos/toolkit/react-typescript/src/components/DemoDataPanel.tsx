9aef612bc50b0a485f41a40e7b5d22882053526f
Tobias Scherer <15127039+tobilebonk@users.noreply.github.com>
2021-02-19 17:38:49 +0100

    Convert DemoDataPanel from class to functional component

--- demos/toolkit/react-typescript/src/components/DemoDataPanel.tsx
+++ demos/toolkit/react-typescript/src/components/DemoDataPanel.tsx
@@ -26,7 +26,7 @@
  ** SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  **
  ***************************************************************************/
-import React, { Component } from 'react'
+import React from 'react'
 import './DemoDescription.css'
 import ItemElement from './ItemElement'
 import plusIcon from '../assets/plus-16.svg'
@@ -39,45 +39,45 @@ interface DemoDataPanelProps {
   onAddNode(): void
 }
 
-export default class DemoDataPanel extends Component<DemoDataPanelProps> {
-  render(): JSX.Element {
-    const nodeItems = this.props.graphData.nodesSource.map(node => (
-      <ItemElement item={node} key={node.id} />
-    ))
+const DemoDataPanel: React.FunctionComponent<DemoDataPanelProps> = (props : DemoDataPanelProps) => {
+  const nodeItems = props.graphData.nodesSource.map(node => (
+    <ItemElement item={node} key={node.id} />
+  ))
 
-    const edgeItems = this.props.graphData.edgesSource.map(edge => (
-      <ItemElement item={edge} key={`${edge.fromNode}-${edge.toNode}`} />
-    ))
+  const edgeItems = props.graphData.edgesSource.map(edge => (
+    <ItemElement item={edge} key={`${edge.fromNode}-${edge.toNode}`} />
+  ))
 
-    return (
-      <div>
-        <h1 className="demo-sidebar-header">Graph Data</h1>
-        <div className="demo-sidebar-content">
-          <p>
-            The following buttons add/remove items in the internal <code>graphData</code> object,
+  return (
+    <div>
+      <h1 className="demo-sidebar-header">Graph Data</h1>
+      <div className="demo-sidebar-content">
+        <p>
+          The following buttons add/remove items in the internal <code>graphData</code> object,
             that is bound to the <code>GraphComponent</code>. Upon change, the graph will be updated
             and rearranged.
           </p>
-          <div className="controls">
-            <button className="control-button" onClick={this.props.onAddNode}>
-              <img src={plusIcon} alt="AddNodeIcon" />
-              <br /> <span>Add Node</span>
-            </button>
-            <button
-              className="control-button"
-              onClick={this.props.onRemoveNode}
-              disabled={this.props.graphData.nodesSource.length === 0}
-            >
-              <img src={minusIcon} alt="RemoveNodeIcon" />
-              <br /> <span>Remove Node</span>
-            </button>
-          </div>
-          <h2>Nodes Source</h2>
-          {nodeItems}
-          <h2>Edges Source</h2>
-          {edgeItems}
+        <div className="controls">
+          <button className="control-button" onClick={props.onAddNode}>
+            <img src={plusIcon} alt="AddNodeIcon" />
+            <br /> <span>Add Node</span>
+          </button>
+          <button
+            className="control-button"
+            onClick={props.onRemoveNode}
+            disabled={props.graphData.nodesSource.length === 0}
+          >
+            <img src={minusIcon} alt="RemoveNodeIcon" />
+            <br /> <span>Remove Node</span>
+          </button>
         </div>
+        <h2>Nodes Source</h2>
+        {nodeItems}
+        <h2>Edges Source</h2>
+        {edgeItems}
       </div>
-    )
-  }
+    </div>
+  )
 }
+
+export default DemoDataPanel;
